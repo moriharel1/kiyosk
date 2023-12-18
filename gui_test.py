8c5b4ffafa0df6a588408898618bd0a8632d817c
jb <68179224+jacblo@users.noreply.github.com>
2023-12-18 18:18:41 +0200

    migrate dealing with scaned code to external function

--- gui_test.py
+++ gui_test.py
@@ -1,6 +1,17 @@
 import PySimpleGUI as sg
 import json
 
+CODES = json.load(open("key.json"))
+
+def deal_with_scan(code):
+    """deals with a barcode scan, and updates the database
+
+    Args:
+        code (str): the barcode scanned (assumes it's in the key.json file)
+    """
+    global CODES
+    
+    print(CODES[code])
 
 def main():
     # Define the layout
@@ -10,7 +21,6 @@ def main():
     ]
 
     # Create the window
-    codes = json.load(open("key.json"))
     window = sg.Window("My Window", layout)
 
     # Event loop
@@ -21,8 +31,8 @@ def main():
             if len(values["barcode"]) and not values["barcode"][-1].isnumeric():
                 window["barcode"].update(values["barcode"][:-1])
 
-            elif values["barcode"] in codes:
-                print(codes[values["barcode"]])
+            elif values["barcode"] in CODES:
+                deal_with_scan(values["barcode"])
                 window["barcode"].update("")
                 
         if event == sg.WINDOW_CLOSED or event == "OK":
