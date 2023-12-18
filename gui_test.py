101bc2076e06edc1bf608e3f0151e1a6204e82a3
jb <68179224+jacblo@users.noreply.github.com>
2023-12-18 18:13:40 +0200

    put into main function

--- gui_test.py
+++ gui_test.py
@@ -1,30 +1,35 @@
 import PySimpleGUI as sg
 import json
 
-# Define the layout
-layout = [
-    [sg.Input("", key="barcode", enable_events=True)],
-    [sg.Button("OK")]
-]
 
-# Create the window
-codes = json.load(open("key.json"))
-window = sg.Window("My Window", layout)
+def main():
+    # Define the layout
+    layout = [
+        [sg.Input("", key="barcode", enable_events=True)],
+        [sg.Button("OK")]
+    ]
 
-# Event loop
-while True:
-    event, values = window.read()
-    print(event, values)
-    if event == "barcode":
-        if len(values["barcode"]) and not values["barcode"][-1].isnumeric():
-            window["barcode"].update(values["barcode"][:-1])
+    # Create the window
+    codes = json.load(open("key.json"))
+    window = sg.Window("My Window", layout)
 
-        elif values["barcode"] in codes:
-            print(codes[values["barcode"]])
-            window["barcode"].update("")
-            
-    if event == sg.WINDOW_CLOSED or event == "OK":
-        break
+    # Event loop
+    while True:
+        event, values = window.read()
+        print(event, values)
+        if event == "barcode":
+            if len(values["barcode"]) and not values["barcode"][-1].isnumeric():
+                window["barcode"].update(values["barcode"][:-1])
 
-# Close the window
-window.close()
+            elif values["barcode"] in codes:
+                print(codes[values["barcode"]])
+                window["barcode"].update("")
+                
+        if event == sg.WINDOW_CLOSED or event == "OK":
+            break
+
+    # Close the window
+    window.close()
+
+if __name__ == "__main__":
+    main()
