bb9e8849de28ef25af0e28275c70e2e06bdec271
moriharel1 <moriharel@gmail.com>
2023-12-20 22:52:57 +0200

    names dictionary

--- util.py
+++ util.py
@@ -5,6 +5,7 @@ TIME_OPERATED = time.strftime("%d/%m/%Y/%H:%M:%S") # the time the program operat
 
 DEALS_F = 'data/deals.json'
 PRODUCTS_F = 'data/products.json'
+NAMES_F = 'data/name_dictionary.json'
 PURCHASE_LOG_F = 'data/purchase_log.csv'
 COUNT_F = 'data/count.csv'
 
@@ -41,6 +42,9 @@ with open(PRODUCTS_F, 'r') as f:
 
 with open(DEALS_F, 'r') as f:
     DEALS_DATA = json.load(f)
+    
+with open(NAMES_F, 'r') as f:
+    NAMES_DATA = json.load(f)
 
 PURCHASE_LOG_DATA = read_file(PURCHASE_LOG_F)
 COUNT_DATA = read_file(COUNT_F)
@@ -50,8 +54,7 @@ COUNT = 0 # count the value of every purchase (the sum of all the purchases)
 def load_products():
     """
     load the products to the file from the user input and save it to the file 
-    this is manager option
-
+    
     :input: barcode, price, name
     """
     while True:
@@ -62,15 +65,49 @@ def load_products():
 
         barcode = input("\nscan product barcode: ")
         price = input("enter product price: ")
-        name = input("enter product name: ")
+        he_name = input("enter product name in hebrew: ")
+        en_name = input("enter product name in english: ")
 
+        #update the products file with the new product barcode price and the name in hebrew
         with open(PRODUCTS_F, 'r') as f:
             data = json.load(f)
-
-        data1 = {barcode : [price, name]}
+        data1 = {barcode : [price, he_name]}
         data.update(data1)
-        
         with open(PRODUCTS_F, 'w') as f:
+            json.dump(data, f)
+
+        #update the names file with the new names of the products in hebrew and english
+        with open(NAMES_F, 'r') as f:
+            data2 = json.load(f)
+        data3 = {he_name : en_name}
+        data2.update(data3)
+        with open(NAMES_F, 'w') as f:
+            json.dump(data2, f)
+
+def load_deals():
+    """
+    load the deals to the file from the user input and save it to the file
+
+    :input: barcode, amount, price
+    """
+
+    while True:
+        #check if user wants to continue
+        inp = input("press enter to continue or q to quit: ")
+        if inp == 'q':
+            break
+
+        barcode = input("\nscan product barcode: ")
+        amount = input("enter amount of products in deal: ")
+        price = input("enter price of the deal: ")
+
+        #update the deals file with the new deal
+        with open(DEALS_F, 'r') as f:
+            data = json.load(f)
+        en_name = NAMES_DATA[PRODUCTS_DATA[barcode][1]]
+        data1 = {en_name : [amount, price]}
+        data.update(data1)
+        with open(DEALS_F, 'w') as f:
             json.dump(data, f)
 
 def price_calculator(barcode, amount):
@@ -82,30 +119,25 @@ def price_calculator(barcode, amount):
     :return: the price of the products
     """
 
-    name_dict = {"\u05d7\u05de\u05e6\u05d5\u05e5" : "hamzoz"}
-
-    product_name = name_dict[PRODUCTS_DATA[barcode][1]]
-    print(product_name)
+    product_name = NAMES_DATA[PRODUCTS_DATA[barcode][1]]
 
     #check if the product is in the deals file
     if product_name in DEALS_DATA:
-        for i in DEALS_DATA[product_name]:
-            print(DEALS_DATA[product_name])
-            print(DEALS_DATA[product_name][i][0])
+        print(DEALS_DATA[product_name])
+        print(DEALS_DATA[product_name][0])
 
-            #check if the amount is enough for the deal
-            if amount >= DEALS_DATA[product_name][i][0]:
-                #calculate the price
-                price = (amount // DEALS_DATA[product_name][0]) * DEALS_DATA[product_name][1] + (amount % DEALS_DATA[product_name][0]) * PRODUCTS_DATA[barcode][0]
-                return price
+        #check if the amount is enough for the deal
+        if amount >= DEALS_DATA[product_name][0]:
+            #calculate the price
+            price = (amount // DEALS_DATA[product_name][0]) * DEALS_DATA[product_name][1] + (amount % DEALS_DATA[product_name][0]) * float(PRODUCTS_DATA[barcode][0])
+            return price
+        
+        else: #if the amount that buyed is not enough for the deal
+            #calculate the price
+            price = amount * float(PRODUCTS_DATA[barcode][0])
+            return price
             
-            else: #if the amount that buyed is not enough for the deal
-                #calculate the price
-                price = amount * PRODUCTS_DATA[barcode][0]
-                return price
-            
-    else: #if the product is not in the deals file calculate the 
-        #calculate the price
+    else: #if the product is not in the deals file calculate the regular price
         price = amount * PRODUCTS_DATA[barcode][0]
         return price
 
@@ -145,14 +177,16 @@ def main():
     #functions check
     #data = read_file(COUNT_F)                         
     #write_file(COUNT_F, [time, time, count])             
-    #load_products()                                   
+    #load_products()  
+    load_deals()                                 
     #buy("1")      
+    #print(price_calculator("1",10))
 
+    #
     #every time do this when the program is done
     #time_end_operate = time.strftime("%d/%m/%Y/%H:%M:%S")
     #write_file(COUNT_F, [time_operated, time_end_operate , COUNT])     
 
-    print(price_calculator("1",1))
     
 
 if __name__ == '__main__':
