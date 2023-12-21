e5d7e8eeeb3109162457fc6b78a643fba360468f
jb <68179224+jacblo@users.noreply.github.com>
2023-12-21 13:48:57 +0200

    updated gui for new data system

--- gui_test.py
+++ gui_test.py
@@ -4,16 +4,17 @@ import util
 from threading import Timer
 import time
 
-CODES = json.load(open("data/products.json"))
-# tuple to have order and set to remove duplicates. sorted to have a consistent order
-DISTINCT_NAMES = sorted(tuple(set([CODES[code][1] for code in CODES])))
+PRICES = json.load(open("data/products.json"))              # name: price
+DISTINCT_NAMES = list(PRICES.keys())
+BARCODES = json.load(open("data/barcodes.json"))            # barcode: name
+LANGUAGE_DICT = json.load(open("data/name_dictionary.json"))# english name: hebrew name
 
-def calc_price_string(code, count):
-    """Calculate the price of a product given its code and count, this is temporary, should be in util
+def calc_price_string(name, count):
+    """Calculate the price of a product given its name and count, this is temporary, should be in util
     then return it as a string with nis symbol"""
     
     # price = util.price_calculator(code, count)
-    price = round(float(CODES[code][0]) * count, 5) # sometimes floating point errors happen so we round
+    price = round(float(PRICES[name]) * count, 5) # sometimes floating point errors happen so we round
     return "₪" + str(price)
 
 # functions to be replaced with util
@@ -28,14 +29,14 @@ def main():
         [sg.Input("", key="barcode", enable_events=True)],
         [sg.Frame("כמות", [[sg.Button("×"+str(i), key="times-"+str(i)) for i in range(1,11)]], title_location='n')]+
             [sg.Frame("אחר",[[sg.Spin([i for i in range(1000)],key="times_custom"), sg.Button("החל", key="times_custom_ok")]], title_location="n")],
-        [sg.Table(  justification="center", values=[], headings=["ברקוד", "פריט", "כמות", "מחיר"], col_widths=[15,13,6,6],
-                    auto_size_columns=False, key="cart")],
+        [sg.Table(  justification="center", values=[], headings=["ברקוד", "name", "פריט", "כמות", "מחיר"], col_widths=[15,0,14,6,6],
+                    auto_size_columns=False, visible_column_map=[True,False,True,True,True], key="cart")], # don't include the english name column
         [sg.Button("סיום קנייה", key="done", bind_return_key=True), sg.Button("הסר פריט", key="remove"), sg.Button("אפס קניה", key="clear")]
     ]
     
-    max_width = max([len(name) for name in DISTINCT_NAMES])
+    max_width = max([len(LANGUAGE_DICT[name]) for name in DISTINCT_NAMES])
     right_col = [
-        [sg.Button(name, key="product-"+str(i), size=(max_width-3,1))]
+        [sg.Button(LANGUAGE_DICT[name], key="product-"+str(i), size=(max_width,1))]
             for i, name in enumerate(DISTINCT_NAMES)
     ]
 
@@ -50,13 +51,13 @@ def main():
     root.destroy()
 
     # Create the window
-    window = sg.Window("My Window", layout, finalize=True, scaling=scaling, element_justification="right")
+    window = sg.Window("My Window", layout, finalize=True, scaling=scaling, element_justification="right", return_keyboard_events=True)
     window.maximize()
     window.bind("<Delete>", "remove")
     window.bind("<Escape>", "reset_cursor")
 
     # State
-    cart = []
+    cart = [] # list of [barcode, english name, hebrew name, count, price]
     last_barcode_key_time = 0
     def barcode_clear_timer(start_time, cur_value):
         nonlocal last_barcode_key_time
@@ -84,8 +85,9 @@ def main():
                     window["barcode"].update(values["barcode"][:-1])
 
                 # this means we're done
-                elif values["barcode"] in CODES:
-                    cart.append([values["barcode"], CODES[values["barcode"]][1], 1, calc_price_string(values["barcode"], 1)])
+                elif values["barcode"] in BARCODES:
+                    name = BARCODES[values["barcode"]]
+                    cart.append([values["barcode"], name, LANGUAGE_DICT[name], 1, calc_price_string(name, 1)])
                     window["barcode"].update("")
             
             case "remove":
@@ -105,23 +107,23 @@ def main():
                 pass
             
             case s if type(s) is str and s.startswith("times-"):
+                count = int(event.split("-")[1])
                 if len(cart) == 0:
                     continue
                 
-                count = int(event.split("-")[1])
-                cart[-1][2] = count
-                cart[-1][3] = calc_price_string(cart[-1][0], count)
+                positions = [-1]
+                if len(values['cart']) != 0:
+                    positions = values['cart']
+                
+                for position in positions:
+                    cart[position][3] = count
+                    cart[position][4] = calc_price_string(cart[position][1], count)
             
-            case s if type(s) is str and s.startswith("product-"):
-                index = int(s.split("-")[1])
+            case s if type(s) is str and (s.startswith("product-") or s.isnumeric()):
+                index = int(s.split("-")[-1])
                 name = DISTINCT_NAMES[index]
-                # find a barcode with that name
-                for code in CODES:
-                    if CODES[code][1] == name:
-                        cart.append([code, name, 1, calc_price_string(code, 1)])
-                        break # add only one
-                else: # none found
-                    print("couldn't find item pressed, ignoring. it was "+s)
+
+                cart.append(["", name, LANGUAGE_DICT[name], 1, calc_price_string(name, 1)])
             
             case "times_custom_ok":
                 if len(cart) == 0:
@@ -129,7 +131,7 @@ def main():
                 
                 count = int(values["times_custom"])
                 cart[-1][2] = count
-                cart[-1][3] = calc_price_string(cart[-1][0], count)
+                cart[-1][3] = calc_price_string(cart[-1][1], count)
             
             case sg.WINDOW_CLOSED:
                 break
