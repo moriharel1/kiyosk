9aad58fcd3c2ba9afdb01bbeaa23a82163f4372c
moriharel1 <moriharel@gmail.com>
2023-12-25 12:39:30 +0200

    add and check things

--- util.py
+++ util.py
@@ -74,24 +74,21 @@ def load_products():
         he_name = input("enter product name in hebrew: ")
 
         #update the barcodes file with the new barcode
-        with open(BARCODES_F, 'r') as f:
-            data = json.load(f)
+        data = BARCODES_DATA
         data1 = {barcode : en_name}
         data.update(data1)
         with open(BARCODES_F, 'w') as f:
             json.dump(data, f)
 
         #update the products file with the new product price
-        with open(PRICES_F, 'r') as f:
-            data = json.load(f)
+        data = PRICES_DATA
         data1 = {en_name : price}
         data.update(data1)
         with open(PRICES_F, 'w') as f:
             json.dump(data, f)
 
         #update the names file with the new names of the products in hebrew and english
-        with open(NAMES_F, 'r') as f:
-            data2 = json.load(f)
+        data2 = NAMES_DATA
         data3 = {en_name : he_name}
         data2.update(data3)
         with open(NAMES_F, 'w') as f:
@@ -115,13 +112,108 @@ def load_deals():
         price = input("enter price of the deal: ")
 
         #update the deals file with the new deal
-        with open(DEALS_F, 'r') as f:
-            data = json.load(f)
+        data = DEALS_DATA
         en_name = BARCODES_DATA[barcode]
         data1 = {en_name : [int(amount), float(price)]}
         data.update(data1)
         with open(DEALS_F, 'w') as f:
             json.dump(data, f)
+
+def change_product_price():
+    """
+    change the price of the product
+
+    :input: barcode, new price
+    """
+
+    barcode = input("\nscan product barcode: ")
+    new_price = input("enter new price: ")
+
+    #update the products file with the new product price
+    data = PRICES_DATA
+    en_name = BARCODES_DATA[barcode]
+    data1 = {en_name : new_price}
+    data.update(data1)
+    with open(PRICES_F, 'w') as f:
+        json.dump(data, f)
+
+def change_product_he_name():
+    """
+    change the hebrew name of the product
+
+    :input: barcode, new hebrew name
+    """
+
+    barcode = input("\nscan product barcode: ")
+    new_he_name = input("enter new hebrew name: ")
+
+    #update the names file with the new hebrew name of the product
+    data = NAMES_DATA
+    en_name = BARCODES_DATA[barcode]
+    data1 = {en_name : new_he_name}
+    data.update(data1)
+    with open(NAMES_F, 'w') as f:
+        json.dump(data, f)
+
+def change_product_en_name():
+    """
+    change the english name of the product
+
+    :input: barcode, new english name
+    """
+
+    barcode = input("\nscan product barcode: ")
+    new_en_name = input("enter new english name: ")
+
+    #update the names file with the new english name of the product
+    data2 = NAMES_DATA
+    data3 = {new_en_name : NAMES_DATA[BARCODES_DATA[barcode]]}
+    data2.update(data3)
+    with open(NAMES_F, 'w') as f:
+        json.dump(data2, f)
+
+    #update the deals file with the new english name of the product
+    data4 = DEALS_DATA
+    data5 = {new_en_name : DEALS_DATA[BARCODES_DATA[barcode]]}
+    data4.update(data5)
+    with open(DEALS_F, 'w') as f:
+        json.dump(data4, f)
+
+    #update the prices file with the new english name of the product
+    data6 = PRICES_DATA
+    data7 = {new_en_name : PRICES_DATA[BARCODES_DATA[barcode]]}
+    data6.update(data7)
+    with open(PRICES_F, 'w') as f:
+        json.dump(data6, f)
+
+    #update the barcodes file with the new english name of the product
+    data = BARCODES_DATA
+    data1 = {barcode : new_en_name}
+    data.update(data1)
+    with open(BARCODES_F, 'w') as f:
+        json.dump(data, f)
+
+
+
+def change_product_deal(): 
+    """
+    change the deal of the product
+
+    :input: barcode, new deal
+    """
+
+    barcode = input("\nscan product barcode: ")
+    new_amount = input("enter new amount of products in deal: ")
+    new_price = input("enter new price of the deal: ")
+
+    #update the deals file with the new deal
+    with open(DEALS_F, 'r') as f:
+        data = json.load(f)
+    en_name = BARCODES_DATA[barcode]
+    data1 = {en_name : [int(new_amount), float(new_price)]}
+    data.update(data1)
+    with open(DEALS_F, 'w') as f:
+        json.dump(data, f)
 
 def price_calculator(name: str, amount: int):
     """
@@ -182,8 +274,13 @@ def main():
     #functions check
     #data = read_file(COUNT_F)                         
     #write_file(COUNT_F, [time, time, count])             
-    #load_products()  
+    load_products()  
     load_deals()                                 
+    change_product_price()
+    change_product_en_name()
+    change_product_he_name()
+    change_product_deal()
+
     #buy("1")      
     #print(price_calculator("1",7))
 
